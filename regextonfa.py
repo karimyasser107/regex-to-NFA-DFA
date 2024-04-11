db6c78e5f61c62ba4ff236bfb8165b51a1361870
karimyasser107 <karimyasser34@gmail.com>
2024-04-11 14:22:08 +0200

    Update regextonfa.py

--- regextonfa.py
+++ regextonfa.py
@@ -1,16 +1,29 @@
 """Convert regular expression to NFA."""
-import re
 
 #start with adding special character to represent Anding
-reg="abc"
-
+reg="a|bc1+3d4(df2)([gh]+2)"
+    #a|b×c×1+×3×d×4×(d×f×2)
 def regextonfa(reg):
     correct_reg=""
-    for i in range(len(reg)-1):
-        correct_reg+=reg[i]
-        if re.match('[a-z]',reg[i]) and re.match('[a-z]',reg[i+1]):
-            correct_reg+="×"
-    correct_reg+=reg[len(reg)-1]
+    for i in range(0,len(reg)-1):
+        if reg[i]=="|" or reg[i+1]=="|":
+            correct_reg+=reg[i]
+            continue
+        elif reg[i+1]=="." or reg[i+1]=="?" or reg[i+1]=="*" or reg[i+1]=="+":
+            correct_reg+=reg[i]+reg[i+1]
+            correct_reg+="×"    
+            continue
+        elif reg[i].isalnum() or reg[i]=="]"or reg[i]==")":
+            if reg[i+1].isalnum() or reg[i+1]=="[" or reg[i+1]=="(":
+                correct_reg+=reg[i]
+                correct_reg+="×"
+                continue
+        if reg[i]=="." or reg[i]=="?" or reg[i]=="*" or reg[i]=="+":
+            continue        
+        else:
+            correct_reg+=reg[i]
+        if i==len(reg)-2:
+            correct_reg+=reg[i+1]
     return correct_reg
 
 print(regextonfa(reg))
