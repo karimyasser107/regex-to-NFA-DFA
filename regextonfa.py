8bc28eca8d001f4090739dae1574275c8cce2b97
karimyasser107 <karimyasser34@gmail.com>
2024-04-14 21:41:15 +0200

    handling ranges + updated validation rules
    + changed representation to list not string

--- regextonfa.py
+++ regextonfa.py
@@ -1,39 +1,97 @@
 """Convert regular expression to NFA."""
 import json
-#start with adding special character to represent Anding
-reg="(a*)*"#"ab(b|c)*d+"#"aa+b*b"#"(a++"
-    #"a|bc1+3d4(df2)(y+2)"
-
+import graphviz
+import pydot
 def validate_reg(reg):
     #check if the input regular expression is valid
     if reg=="":
         return False
-    if reg[0]=="*" or reg[0]=="+" or reg[0]=="?" or reg[0]=="×" or reg[0]=="|" or reg[0]=="-":
+    if reg[0]=="*" or reg[0]=="+" or reg[0]=="?"  :
         #example "*abc" is invalid
         return False
-    if reg[-1]=="|":
-        #example "a|b|c|"" is invalid
-        return False
+    # if reg[-1]=="|":
+    #     #example "a|b|c|"" is invalid
+    #     return False
     for i in range(0,len(reg)-1):
-        if reg[i]=="*" or reg[i]=="+" or reg[i]=="?" or reg[i]=="|":
-            if reg[i+1]=="*" or reg[i+1]=="+" or reg[i+1]=="?" or reg[i+1]=="|":
-                #example "a|b||c" is invalid
+        if reg[i]=="*":##error reg[i]=="+" when 3  w kaman reg[i]=="?
+            if reg[i+1]==reg[i]:
+                #example "a|b**c" is invalid
                 return False
-        if reg[i]=="-":
-            if reg[i+1].isalnum() and reg[i-1].isalnum():
+        if i<len(reg)-2 and ((reg[i]=="+" and reg[i+1]=="+" and reg[i+2]=="+")or(reg[i]=="?" and reg[i+1]=="?" and reg[i+2]=="?")):
+            #example "a+++" or "a???" is invalid
+            return False
+        if reg[i]=="+" and reg[i+1]=="*" :
+            #example "a+*" is invalid
+            return False
+        if reg[i]=="?" and reg[i+1]=="*" :
+            #example "a?*" is invalid
+            return False
+        if reg[i]=="|" :
+            if reg[i+1]=="*" or reg[i]=="?" or reg[i+1]=="+":
+                #example "a|b|+c" or "a|b|+*" is invalid
+                return False
+        
+        if reg[i]=="-":#TODO checkkkkk if all alphanumeric
+            if i>0 and reg[i+1].isalnum() and reg[i-1].isalnum():
+                continue
+            return False
+        if reg[i]=="[":
+            if reg[i+1]=="]":#[] invalid
                 return False
     return True
+def validate_brackets(reg):
+    stack=[]
+    for char in reg:
+        if char=="(":
+            stack.append(char)
+        elif char==")":
+            #stack[-1] is the last element in the stack
+            while stack and stack[-1]!="(":
+                stack.pop()
+            if stack:
+                stack.pop()
+            else:
+                return "unbalanced parentheses"
+        elif char=="[":
+            stack.append(char)
+        elif char=="]":
+            #stack[-1] is the last element in the stack
+            while stack and stack[-1]!="[":
+                stack.pop()
+            if stack:
+                stack.pop()
+            else:
+                return "unbalanced parentheses"
+    if stack:
+        return "unbalanced parentheses"
+        
+    return "balanced parentheses"
+#start with adding special character to represent Anding
 def regex_anding(reg):
-    correct_reg=""
+    correct_reg=[]
+    range_str=""
     is_range=False
     last_is_put=False
+    first_is_operator_and_already_put=False
     # take care this loop is from 0 to len(reg)-2 so concentarte to know how we handle the last character 
     for i in range(0,len(reg)-1):
+        if is_range:
+            if reg[i]=="]":
+                is_range=False
+                correct_reg.append(range_str)
+                range_str=""
+            else:
+                range_str+=reg[i]
+                continue
         if reg[i]=="|" or reg[i+1]=="|":
-            correct_reg+=reg[i]
+            correct_reg.append(reg[i])
             continue
-        elif reg[i+1]=="." or reg[i+1]=="?" or reg[i+1]=="*" or reg[i+1]=="+":
-            correct_reg+=reg[i]+reg[i+1]
+        elif reg[i+1]=="?" or reg[i+1]=="*" or reg[i+1]=="+":
+            if not first_is_operator_and_already_put:
+                correct_reg.append(reg[i])
+            else:
+                first_is_operator_and_already_put=False
+            correct_reg.append(reg[i+1])
             # this is the only case where we put TWO chars together
             # so, if we reached the end of the regular expression,
             # we dont want to put the last character (which is always an operator as we are in this elif) again
@@ -42,34 +100,67 @@ def regex_anding(reg):
             if i==len(reg)-2:
                 last_is_put=True
                 break
-            correct_reg+="×"    
-            continue
-        elif reg[i].isalnum() or reg[i]=="]"or reg[i]==")":
-            if reg[i+1].isalnum() or reg[i+1]=="[" or reg[i+1]=="(":
-                correct_reg+=reg[i]
-                correct_reg+="×"
+            #TODO check if the next character is an operator if yes dont put anding
+            if i<len(reg)-2 and (reg[i+2]=="*" or reg[i+2]=="+" or reg[i+2]=="?" or reg[i+2]=="|"):#example "a*?" we dont want to put anding
+                first_is_operator_and_already_put=True #if we have multiple operators together we dont want to put them twice each
                 continue
-        # elif reg[i]=="[" :
-        #     correct_reg+=reg[i]
-        #     is_range=True
-        #     continue
-        if reg[i]=="." or reg[i]=="?" or reg[i]=="*" or reg[i]=="+":
+            correct_reg.append("×")    
+            continue
+        # elif reg[i]=="]":
+        #     if reg[i+1].isalnum() or reg[i+1]=="[" or reg[i+1]=="(":
+        #         #we will not put the "]" in the correct_reg as it is already put as a string (range_str) in the correct_reg
+        #         correct_reg.append("×")
+        #         continue
+        elif reg[i].isalnum() or reg[i]=="]" or reg[i]==")":
+            if reg[i+1].isalnum() or reg[i+1]=="[" or reg[i+1]=="(":
+                correct_reg.append(reg[i])
+                correct_reg.append("×")
+                continue
+        elif reg[i]=="[" :
+            correct_reg.append(reg[i])
+            range_str=""
+            is_range=True
+            continue
+        if reg[i]=="?" or reg[i]=="*" or reg[i]=="+":
             continue        
         else:
-            correct_reg+=reg[i]
+            correct_reg.append(reg[i])
     # put the last character in the regular expression
     # only if last_is_put is False
     if i==len(reg)-2 and not last_is_put:
-        correct_reg+=reg[i+1]
-    return correct_reg
+        if is_range:#this means that the regex was ending with the "]" example 1[a-z] this means that we didnot put the range_str in the correct_reg
+                    #as is_range is still True
+            correct_reg.append(range_str)
+        correct_reg.append(reg[i+1])
+    return correct_reg #the result could be ['[', 'a-z_', ']', '[', 'a-z0-9_', ']', '*'] then we want to concat the range in one string (function concat_range)
+
+def concat_range(correct_reg):
+    reg=[]
+    range_str=""
+    is_range=False
+    for i in range(0,len(correct_reg)):
+        if correct_reg[i]=="]":
+            continue
+        if is_range:
+            range_str="["+correct_reg[i]+"]"
+            if correct_reg[i+1]!="]":
+                return ["%"]#just to mark that the regular expression is invalid
+            reg.append(range_str)
+            is_range=False
+        elif correct_reg[i]=="[" :
+            is_range=True
+        else:
+            reg.append(correct_reg[i])
+    return reg
+        
 
 def remove_extra_anding(reg):
-    correct_regex=""
+    correct_regex=[]
     for i in range(0, len(reg)-1):
         if (reg[i]=="×" and reg[i+1]==")"):
             continue
-        correct_regex+=reg[i]
-    correct_regex+=reg[len(reg)-1]
+        correct_regex.append(reg[i])
+    correct_regex.append(reg[len(reg)-1])
     return correct_regex
 
 def precedence(operator):
@@ -88,8 +179,7 @@ def precedence(operator):
 def infix_to_postfix(infix):
     operators=["*","+","?","×","|"]
     stack=[]
-    postfix=""
-    is_range=False
+    postfix=[]
     for char in infix:
         # if char.isalnum() or char=="×":
         #     postfix+=char
@@ -98,41 +188,22 @@ def infix_to_postfix(infix):
         elif char==")":
             #stack[-1] is the last element in the stack
             while stack and stack[-1]!="(":
-                postfix+=stack.pop()
-            if stack:
-                stack.pop()
-            else:
-                return "unbalanced parentheses"
-        elif char=="[":
-            is_range=True
-            stack.append(char)
-        elif char=="]":
-            is_range=False
-            #stack[-1] is the last element in the stack
-            while stack and stack[-1]!="[":
-                postfix+=stack.pop()
+                postfix.append(stack.pop())
             if stack:
                 stack.pop()
             else:
                 return "unbalanced parentheses"
         elif char in operators:
             while stack and precedence(char)<=precedence(stack[-1]):
-                postfix+=stack.pop()
+                postfix.append(stack.pop())
             stack.append(char)
-        elif char=="-":
-            #if the character is a range example [a-z]
-            #TODO
-            postfix+=char
         else:
-            #if char is a alphanumeric character
-            postfix+=char
-            if is_range:
-                #if the character is in a range example [abc]
-                postfix+="×"
+            #if char is a alphanumeric character or a range string example "[a-z]"
+            postfix.append(char)
     while stack:
-        if stack[-1]=="(" or stack[-1]=="[":
+        if stack[-1]=="(":
             return "unbalanced parentheses"
-        postfix+=stack.pop()
+        postfix.append(stack.pop())
     return postfix
 
 
@@ -305,7 +376,6 @@ def create_json(nfa_dict):
     print("json file created successfully")
     
 # Draw the NFA graph using Graphviz
-import graphviz
 
 def draw_nfa(json_filename):
     # Load the NFA data from the JSON file
@@ -313,14 +383,22 @@ def draw_nfa(json_filename):
         nfa_data = json.load(json_file)
 
     # Create a new Graphviz graph
-    graph = graphviz.Digraph(format='png')
+    graph = graphviz.Digraph(format='png', name='NFA_graph', graph_attr={'rankdir': 'LR'})
+    # graph = pydot.Dot("NFA_graph",graph_type='digraph')
+    # node=pydot.Node("S"+str(nfa_data['startingState']),label="S"+str(nfa_data['startingState']),shape='circle')
 
+    start_state=None
     # Add nodes for each state
     for state, state_data in nfa_data.items():
         if state == 'startingState':
-            continue
+            graph.node(state_data, label=state_data, shape= 'circle')
+            start_state=state_data
+            #add an edge to point to start state
+            graph.node('None', label='', style='invisible')
+            graph.edge('None', start_state)
         else:
-            graph.node(state, label=state, shape='doublecircle' if state_data['isTerminatingState'] else 'circle')
+            if state!=start_state:
+                graph.node(state, label=state, shape='doublecircle' if state_data['isTerminatingState'] else 'circle')
 
     # Add edges for transitions
     for state, state_data in nfa_data.items():
@@ -338,25 +416,37 @@ def draw_nfa(json_filename):
     
 
 
-
+reg="[a-c]*"#"(a+a+)+b" #"(a*b*)([a-b]*)"#"(a*)*"#"(a*b)(b?a+)" "(a*?)*" #"([)" #"[a-z_][a-z0-9_]*[!?]?" #"(a|b)*bc+" #"((00)|1)*1(0|1)" #"(a*)*" #"ab(b|c)*d+" #"aa+b*b" #"(a++"
+    #"a|bc1+3d4(df2)(y+2)"
 #validate the regular expression
 if not validate_reg(reg):
     print("invalid regular expression")
+    exit()
+#validate the brackets
+if validate_brackets(reg)=="unbalanced parentheses":
+    print("unbalanced parentheses")
     exit()
 print("\n======")
 print("after adding anding the regular expression")
 regex_anded=regex_anding(reg)
 print(regex_anded)
 print("\n======")
+print("after concating the range in one string")
+correct_regex_with_range=concat_range(regex_anded)
+if correct_regex_with_range==["%"]:
+    print("invalid range expression")
+    exit()
+print(correct_regex_with_range)
+print("\n======")
 print("after removing extra anding")
-correct_regex=remove_extra_anding(regex_anded)
+correct_regex=remove_extra_anding(correct_regex_with_range)
 print(correct_regex)
 print("\n======")
 postfix=infix_to_postfix(correct_regex)
 if postfix=="unbalanced parentheses":
     print("unbalanced parentheses")
     exit()
-print("after converting the regular expression to postfix")
+print("after converting the regular expression to postfix")#['[a-z_]', '×', '[a-z0-9_]', ']*', '×', '[!?]', ']?']
 print(postfix)
 print("\n======")
 
